196762e4d92060979765d01a9bdc7886f996e8a0
iso <jigom11791>
2022-11-14 13:38:59 -0800

    added models

--- SecretSanta/SecretSantaApp/models.py
+++ SecretSanta/SecretSantaApp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Participants(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    email = models.CharField(max_length=300)
+    selected = models.BooleanField(default=False)
+    suggestions = models.TextField()
+
+    def __str__(self):
+        return self.name
+    
+    def is_selected(self):
+        return self.selected
+    
+    def select(self):
+        if not self.select:
+            self.select = True
+            return True
+        else:
+            return False
